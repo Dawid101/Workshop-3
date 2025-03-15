051145229c26b9c8a852a0da0f4f8714fcdd570b
dawid101 <dawcik35@gmail.com>
2025-03-15 17:54:20 +0100

    pokazanie użytkownika

---
+++ jee-user-crud/src/main/java/pl/coderslab/users/UserShow.java
@@ -0,0 +1,23 @@
+package pl.coderslab.users;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import pl.coderslab.entity.User;
+import pl.coderslab.entity.UserDao;
+
+import java.io.IOException;
+
+@WebServlet("/user/show")
+public class UserShow extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int id = Integer.parseInt(req.getParameter("id"));
+        UserDao userDao = new UserDao();
+        User user = userDao.read(id);
+        req.setAttribute("user", user);
+        getServletContext().getRequestDispatcher("/users/show.jsp").forward(req, resp);
+    }
+}
